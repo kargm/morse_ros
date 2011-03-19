fa250700a265d313c258a4719dec4afdb773eb58
Thibault Kruse <kruset@in.tum.de>
2011-03-20 00:36:11 +0100

    code compiles

--- human_nav_node/src/NavigationNode.cpp
+++ human_nav_node/src/NavigationNode.cpp
@@ -5,20 +5,29 @@
  *      Author: thibault
  */
 
-#include <human_nav_node/NavigationNode.h>
-
-NavigationNode::NavigationNode() {}
-
-NavigationNode::~NavigationNode() {}
+#include "ros/ros.h"
 
 
+
+//
+//bool plan(beginner_tutorials::AddTwoInts::Request  &req,
+//         beginner_tutorials::AddTwoInts::Response &res )
+//{
+//
+//}
+
+bool plan() {
+  return false;
+}
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "hanp_plan_server");
   ros::NodeHandle n;
 
-  ros::ServiceServer service = n.advertiseService("hanp_plan", add);
+//  NavigationNode *node = new NavigationNode();
+
+//  ros::ServiceServer service = n.advertiseService("hanp_plan", plan);
   ROS_INFO("Ready to plan.");
   ros::spin();
 
