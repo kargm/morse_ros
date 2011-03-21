c3862cb8336cedddfb31e43a72c1ba4db81cac18
Thibault Kruse <kruset@in.tum.de>
2011-03-21 17:16:49 +0100

    removed dependency to H2

--- human_nav_node/include/mhpError.h
+++ human_nav_node/include/mhpError.h
@@ -1,42 +1,15 @@
 
-/* 
- * Copyright (c) 1993-2005 LAAS/CNRS
- * All rights reserved.
- *
- * Redistribution and use  in source  and binary  forms,  with or without
- * modification, are permitted provided that the following conditions are
- * met:
- *
- *   1. Redistributions of  source  code must retain the  above copyright
- *      notice, this list of conditions and the following disclaimer.
- *   2. Redistributions in binary form must reproduce the above copyright
- *      notice,  this list of  conditions and the following disclaimer in
- *      the  documentation  and/or  other   materials provided  with  the
- *      distribution.
- *
- * THIS  SOFTWARE IS PROVIDED BY  THE  COPYRIGHT HOLDERS AND CONTRIBUTORS
- * "AS IS" AND  ANY  EXPRESS OR IMPLIED  WARRANTIES,  INCLUDING,  BUT NOT
- * LIMITED TO, THE IMPLIED WARRANTIES  OF MERCHANTABILITY AND FITNESS FOR
- * A PARTICULAR  PURPOSE ARE DISCLAIMED. IN  NO EVENT SHALL THE COPYRIGHT
- * HOLDERS OR      CONTRIBUTORS  BE LIABLE FOR   ANY    DIRECT, INDIRECT,
- * INCIDENTAL,  SPECIAL,  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
- * BUT NOT LIMITED TO, PROCUREMENT OF  SUBSTITUTE GOODS OR SERVICES; LOSS
- * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN  CONTRACT, STRICT LIABILITY, OR
- * TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
- * USE   OF THIS SOFTWARE, EVEN   IF ADVISED OF   THE POSSIBILITY OF SUCH
- * DAMAGE.
- */
-
-/*------------------  Fichier généré automatiquement ------------------*/
-/*------------------  Ne pas éditer manuellement !!! ------------------*/
-
-
 #ifndef mhp_ERROR_H
 #define mhp_ERROR_H
 
-#include "h2errorLib.h"
-#include "genom/genomError.h"
+// TODO, use H2 or something
+//#include "h2errorLib.h"
+//#include "genom/genomError.h"
+
+/* M_id and err are encoded on 'signed short' (ie, < 2^15 = 32768) and 'short'
+ *               S_lib_error = (M_lib << 16 | err)
+ */
+#define H2_ENCODE_ERR(M_id,err)   (M_id << 16 | (err&0xffff))
 
 #define M_mhp    (1453)
 
