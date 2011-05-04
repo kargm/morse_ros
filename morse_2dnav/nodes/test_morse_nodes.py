c00bbdf02add2e69368786b8455ba50e00ebb775
kargm <kargm@in.tum.de>
2011-05-04 14:13:38 +0200

    Added testnode to test for needed rosnodes and adapted launchfiles for using it

---
+++ morse_2dnav/nodes/test_morse_nodes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python 
+import roslib
+roslib.load_manifest('rosnode')
+roslib.load_manifest('rospy')
+import rospy
+#from rospy import ROSException
+from rosnode import rosnode_ping
+from rospy import logerr
+
+if __name__ == '__main__':
+
+    if rosnode_ping("morse", 1, 0) == 1:
+        pass
+    else: 
+        logerr("Could not find MORSE-rosnode. Please make sure it is running.")
+
+    if rosnode_ping("morse_tf_broadcaster_navstack", 1, 0) == 1:
+        pass
+    else: 
+        logerr("Could not find MORSE-TF-BROADCASTER. Please make sure it is running.")
+
+    if rosnode_ping("robot_state_publisher", 1, 0) == 1:
+        pass
+    else: 
+        logerr("Could not find ROBOT_STATE_PUBLISHER. Please make sure it is running and the JointStates are published on the correct topic!")
