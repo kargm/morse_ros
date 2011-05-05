c3052e2e86df49ee7ad4a4ef6afb6dc94d6ffbb7
kargm <kargm@in.tum.de>
2011-05-05 12:18:12 +0200

    Fixed namespace in launch-files and fixed node-test

--- morse_2dnav/nodes/test_morse_nodes.py
+++ morse_2dnav/nodes/test_morse_nodes.py
@@ -9,17 +9,17 @@ from rospy import logerr
 
 if __name__ == '__main__':
 
-    if rosnode_ping("morse", 1, 0) == 1:
+    if rosnode_ping("morse", 5, 0) == 1:
         pass
     else: 
         logerr("Could not find MORSE-rosnode. Please make sure it is running.")
 
-    if rosnode_ping("morse_tf_broadcaster_navstack", 1, 0) == 1:
+    if rosnode_ping("morse_tf_broadcaster_navstack", 5, 0) == 1:
         pass
     else: 
         logerr("Could not find MORSE-TF-BROADCASTER. Please make sure it is running.")
 
-    if rosnode_ping("robot_state_publisher", 1, 0) == 1:
+    if rosnode_ping("robot_state_publisher", 5, 0) == 1:
         pass
     else: 
         logerr("Could not find ROBOT_STATE_PUBLISHER. Please make sure it is running and the JointStates are published on the correct topic!")
