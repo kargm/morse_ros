9334d0a012f63744f43433367e8d21ce06c8d29d
Thibault Kruse <kruset@in.tum.de>
2011-05-05 14:08:40 +0200

    files that should not be in repo

--- morse_jido_manipulation_action/src/mjido-manip-as/msg/__init__.py
+++
@@ -1,7 +0,0 @@
-from ._arm_manipulationActionGoal import *
-from ._arm_manipulationActionResult import *
-from ._arm_manipulationActionFeedback import *
-from ._arm_manipulationAction import *
-from ._arm_manipulationGoal import *
-from ._arm_manipulationFeedback import *
-from ._arm_manipulationResult import *
