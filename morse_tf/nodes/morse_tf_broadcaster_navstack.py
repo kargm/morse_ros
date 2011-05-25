1025681fc039c94be5e8f99a785183db6787e13a
kargm <kargm@in.tum.de>
2011-05-25 17:19:35 +0200

    trying to get clean navigation stack bindings...

--- morse_tf/nodes/morse_tf_broadcaster_navstack.py
+++ morse_tf/nodes/morse_tf_broadcaster_navstack.py
@@ -11,13 +11,22 @@ import time
 import math
 import tf
 import geometry_msgs
-#import turtlesim.msg
 import std_msgs
 import nav_msgs
 from nav_msgs.msg import Odometry
 from rosgraph_msgs.msg import Clock
-from geometry_msgs.msg import PoseStamped
+from geometry_msgs.msg import Pose
 from geometry_msgs.msg import Vector3Stamped
+
+# Global variables to store the robot starting position in the /map frame for building the /odom frame
+odom_init = 0
+odom_pose_x = 0
+odom_pose_y = 0
+odom_pose_z = 0
+odom_quat_x = 0
+odom_quat_y = 0
+odom_quat_z = 0
+odom_quat_w = 0
 
 def handle_human_pose(msg, robotname):
     now = rospy.Time.now()
@@ -30,38 +39,82 @@ def handle_human_pose(msg, robotname):
                      
 def handle_map_odom_init(msg, robotname):
     br3 = tf.TransformBroadcaster()
-    
-    br3.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, 0),
-                    (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w),
+
+    global odom_init
+    global odom_pose_x
+    global odom_pose_y
+    global odom_pose_z
+    global odom_quat_x
+    global odom_quat_y
+    global odom_quat_z
+    global odom_quat_w
+
+    # To obtain the initial point of the odom-frame, the global coordinates are set with the first message
+    if odom_init == 0:
+        odom_pose_x = msg.pose.pose.position.x
+        odom_pose_y = msg.pose.pose.position.y
+        odom_pose_z = msg.pose.pose.position.z
+        odom_quat_x = msg.pose.pose.orientation.x
+        odom_quat_y = msg.pose.pose.orientation.y
+        odom_quat_z = msg.pose.pose.orientation.z
+        odom_quat_w = msg.pose.pose.orientation.w
+        odom_init = 1
+
+    # publish the odom init
+    br3.sendTransform((odom_pose_x, odom_pose_y, odom_pose_z),
+                    (odom_quat_x, odom_quat_y, odom_quat_z, odom_quat_w),
                     rospy.Time.now(),
                     "/odom",
                     "/map")
+    # transformation bewteen map and robot frame
+    br3.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, msg.pose.pose.position.z),
+                     (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w),
+                     rospy.Time.now(),
+                     "/base_footprint",
+                     "/map")
+                                    
+    
     
 def handle_odometry(msg, robotname):
     br = tf.TransformBroadcaster()
     now = rospy.Time.now()
     
-    br.sendTransform((msg.pose.pose.position.x, msg.pose.pose.position.y, 0),
-                      (msg.pose.pose.orientation.x, msg.pose.pose.orientation.y, msg.pose.pose.orientation.z, msg.pose.pose.orientation.w),
+    global robot_pose_x
+    global robot_pose_y
+    global robot_pose_z
+    global robot_quat_x
+    global robot_quat_y
+    global robot_quat_z
+    global robot_quat_w
+
+    robot_pose_x += robot_pose_x + msg.position.x/100
+    robot_pose_y += robot_pose_y + msg.position.y/100
+    robot_pose_z += robot_pose_z + msg.position.z/100
+    robot_quat_x += msg.orientation.x
+    robot_quat_y += msg.orientation.y
+    robot_quat_z += msg.orientation.z
+    robot_quat_w += msg.orientation.w
+
+    br.sendTransform((robot_pose_x, robot_pose_y, robot_pose_z),
+                      (robot_quat_x, robot_quat_y, robot_quat_z, robot_quat_w),
                       rospy.Time.now(),
                       "/base_footprint",
                       "/odom")
 
 if __name__ == '__main__':
     rospy.init_node('morse_tf_broadcaster')
-    init = 0
     
     # Initialize odom-frame with robot starting-position
-    rospy.Subscriber('/odom_init',
+    rospy.Subscriber('/Jido/Pose_sensor',
                      nav_msgs.msg.Odometry,
                      handle_map_odom_init,
-                     '/odometry_init')
+                     '/Jido/Pose_sensor')
 
     # Odometry information frame                      
-    rospy.Subscriber('/odom',
-                     nav_msgs.msg.Odometry,
-                     handle_odometry, 
-                     '/odometry_information')
+    #rospy.Subscriber('/Jido/Odometry',
+    #                 geometry_msgs.msg.Pose,
+    #                 handle_odometry, 
+    #                 '/Jido/Odometry')
 
     # Ground-truth frame for Human Position
     rospy.Subscriber('/Human/GPS',
