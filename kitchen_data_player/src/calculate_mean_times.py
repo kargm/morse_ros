cb46058b959f93fd8270efa8dbbe4e3d709aef87
Michael Karg <kargm@in.tum.de>
2012-01-18 14:30:05 +0100

    Output files for mean times

--- kitchen_data_player/src/calculate_mean_times.py
+++ kitchen_data_player/src/calculate_mean_times.py
@@ -34,28 +34,45 @@ stove_counter = 0
 nav_sum = 0
 nav_counter = 0
 
+# write different files of locations for creating gaussians over time
+TABLE_FILE = open("table.csv","w")
+STOVE_FILE = open("stove.csv","w")
+DRAWER_FILE = open("drawer.csv","w")
+CUPBOARD_FILE = open("cupboard.csv","w")
+NAV_FILE = open("navigation.csv","w")
+TABLE_FILE.write("instance,location,duration\n")
+STOVE_FILE.write("instance,location,duration\n")
+DRAWER_FILE.write("instance,location,duration\n")
+CUPBOARD_FILE.write("instance,location,duration\n")
+NAV_FILE.write("instance,location,duration\n")
+
 for row in objReader:
     # Write new csv-file with: instance, time, BECX, BEXY, BECTheta, 
    
     if (row['location'] == 'table'):
         table_sum += float(row['duration'])
         table_counter += 1
+        TABLE_FILE.write("%s,%s,%s\n"%(row['instance'], row['location'], row['duration']))
 
     if (row['location'] == 'stove'):
         stove_sum += float(row['duration'])
         stove_counter += 1
+        STOVE_FILE.write("%s,%s,%s\n"%(row['instance'], row['location'], row['duration']))
 
     if (row['location'] == 'drawer'):
         drawer_sum += float(row['duration'])
         drawer_counter += 1
+        DRAWER_FILE.write("%s,%s,%s\n"%(row['instance'], row['location'], row['duration']))
 
     if (row['location'] == 'cupboard'):
         cupboard_sum += float(row['duration'])
         cupboard_counter += 1
+        CUPBOARD_FILE.write("%s,%s,%s\n"%(row['instance'], row['location'], row['duration']))
 
     if (row['location'] == 'navigation'):
         nav_sum += float(row['duration'])
         nav_counter += 1
+        NAV_FILE.write("%s,%s,%s\n"%(row['instance'], row['location'], row['duration']))
 
 
 table_mean = table_sum / table_counter
