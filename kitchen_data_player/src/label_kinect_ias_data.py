617c430d66581e3beb4a207d997a14ef8bef2a28
Michael Karg <kargm@in.tum.de>
2012-02-03 17:55:47 +0100

    Fixed furniture names in labelling script

--- kitchen_data_player/src/label_kinect_ias_data.py
+++ kitchen_data_player/src/label_kinect_ias_data.py
@@ -77,7 +77,7 @@ def get_cluster_number(x, y):
 
     if p1 > p2 and p1 > p3 and p1 > p4 and p1 > threshhold:
         cluster = 1
-        location = 'drawer'
+        location = 'drawer55'
         p = p1/p1_max
         #print("Human is in cluster %s with prob: %s"%(location, p1))
     elif p2 > p1 and p2 > p3 and p2 > p4 and p2 > threshhold:
@@ -87,12 +87,13 @@ def get_cluster_number(x, y):
         #print("Human is in cluster %s with prob: %s"%(location, p2))
     elif p3 > p1 and p3 > p2 and p3 > p4 and p3 > threshhold:
         cluster = 3
-        location = 'stove'
+        #location = 'stove'
+        location = 'dishwasher'
         p = p3/p3_max
         #print("Human is in cluster %s with prob: %s"%(location, p3))
     elif p4 > p1 and p4 > p2 and p4 > p3 and p4 > threshhold:
         cluster = 4
-        location = 'cupboard'
+        location = 'drawer115'
         p = p4/p4_max
         #print("Human is in cluster %s with prob: %s"%(location, p4))
     else:
