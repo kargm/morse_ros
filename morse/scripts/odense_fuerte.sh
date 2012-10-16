94a9d9d2b4a9135214378532742beb9155e98eaf
Michael Karg <kargm@atbeetz21.(none)>
2012-10-16 16:47:08 +0200

    Added Fuerte kitchen script

---
+++ morse/scripts/odense_fuerte.sh
@@ -0,0 +1,40 @@
+#! /usr/bin/env sh
+
+if [ -n "$ROS_PACKAGE_PATH" ]
+then
+  echo "Using ROS_PACKAGE_PATH: $ROS_PACKAGE_PATH"
+else
+  echo ROS_PACKAGE_PATH not set
+fi
+
+PYYAML_DIR=$(rospack find morse_pyyaml)
+MORSE_DIR=$(rospack find morse)
+ROSLIB_DIR=$(rospack find roslib)
+BLENDER_DIR=$(rospack find morse_blender)
+
+# set python version for morse
+PYTHON_VERSION=python3.2
+
+if [ -z "$PYTHON3_EXECUTABLE" ]; then
+    if which python3.2; then
+        PYTHON3_EXECUTABLE=`which python3.2`
+    else
+        echo "Could not find python3 executable. Please set the environment variable PYTHON3_EXECUTABLE to point to it."
+        exit 1
+    fi
+fi
+
+PYTHON_INST_DIR=`$PYTHON3_EXECUTABLE -c "import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_lib(0,0,''))"`
+export PYTHONPATH=$PYYAML_DIR/pyyaml/lib/python3.2/site-packages:$MORSE_DIR/morse/$PYTHON_INST_DIR:/usr/$PYTHON_INST_DIR:/opt/ros/fuerte/lib/python2.7/dist-packages
+
+
+export MORSE_BLENDER=$BLENDER_DIR/bin/blender
+
+echo "Using PYTHONPATH: $PYTHONPATH"
+
+
+echo "In case you have blend files left, attempting to delete."
+
+$MORSE_DIR/morse/bin/morse edit $MORSE_DIR/morse-src/examples/tutorials/ros_navigation/scenario.py
+# Start using builder script
+#$MORSE_DIR/morse/bin/morse exec $MORSE_DIR/morse/share/examples/morse/scenarii/adapto_rebuild/ias_kitchen_big.py
