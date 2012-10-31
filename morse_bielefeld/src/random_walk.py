7964c778f38f54d3ec91499ac41c3b672b6a1ce9
Michael Karg <kargm@atbeetz21.(none)>
2012-10-31 17:46:43 +0100

    Added random choice of targets

--- morse_bielefeld/src/random_walk.py
+++ morse_bielefeld/src/random_walk.py
@@ -8,11 +8,10 @@ from move_base_msgs.msg import *
 # Brings in the SimpleActionClient
 import actionlib
 
-# Brings in the messages used by the fibonacci action, including the
-# goal message and the result message.
-#import actionlib_tutorials.msg
+from random import randint
 
 def start_position():
+    print("Going to starting position...")
     client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
     client.wait_for_server()
     goal = MoveBaseGoal()
@@ -21,12 +20,12 @@ def start_position():
     goal.target_pose.pose.position.x = -3
     goal.target_pose.pose.position.y = 0
     goal.target_pose.pose.orientation.w = 1.0;
-    print("Sending the robot to cupboard 2...(timeout=5sec)")
     client.send_goal(goal)
     client.wait_for_result()
     return client.get_result()
 
 def cupboard_1():
+    print("Going to cupboard 1")
     client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
     client.wait_for_server()
     goal = MoveBaseGoal()
@@ -35,12 +34,12 @@ def cupboard_1():
     goal.target_pose.pose.position.x = 2
     goal.target_pose.pose.position.y = 2
     goal.target_pose.pose.orientation.w = 1.0;
-    print("Sending the robot to cupboard 2...(timeout=5sec)")
     client.send_goal(goal)
     client.wait_for_result()
     return client.get_result()
 
 def cupboard_2():
+    print("Going to cupboard 2")
     client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
     client.wait_for_server()
     goal = MoveBaseGoal()
@@ -49,23 +48,46 @@ def cupboard_2():
     goal.target_pose.pose.position.x = 2
     goal.target_pose.pose.position.y = 0
     goal.target_pose.pose.orientation.w = 1.0;
-    print("Sending the robot to cupboard 2...(timeout=5sec)")
     client.send_goal(goal)
     client.wait_for_result()
     return client.get_result()
+
+def cupboard_3():
+    print("Going to cupboard 3")
+    client = actionlib.SimpleActionClient('/move_base', MoveBaseAction)
+    client.wait_for_server()
+    goal = MoveBaseGoal()
+    goal.target_pose.header.stamp = rospy.Time.now()
+    goal.target_pose.header.frame_id = "map"
+    goal.target_pose.pose.position.x = 2
+    goal.target_pose.pose.position.y = -3
+    goal.target_pose.pose.orientation.w = 1.0;
+    client.send_goal(goal)
+    client.wait_for_result()
+    return client.get_result()
+
+# Walk around between randomly chosen cupboard and starting position
+def go_to_random_cupboard_and_back():
+    rnd = randint(1,3)
+    print("Selected Cupboard %s\n"%rnd)
+    if rnd == 1:
+        cupboard_1()
+    elif rnd == 2:
+        cupboard_2()
+    elif rnd == 3:
+        cupboard_3()
+    start_position()
+    return 0
 
 if __name__ == '__main__':
     try:
         # Initializes a rospy node so that the SimpleActionClient can
         # publish and subscribe over ROS.
         rospy.init_node('random_walk_client')
-
-        result = cupboard_1()
-        print("Reached CB1: " + str(result))
-        result = start_position()
-        print("Reached Start: " + str(result))
-        result = cupboard_2()
-        print("Reached CB2: " + str(result))
+        i = 0
+        while i < 100:
+            go_to_random_cupboard_and_back()
+            i = i + 1
 
     except rospy.ROSInterruptException:
         print "program interrupted before completion"
