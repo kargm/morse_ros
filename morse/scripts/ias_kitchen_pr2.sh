4e958f78e6e2f8482c6d484920b146e444c6fb76
Michael Karg <kargm@in.tum.de>
2013-04-12 12:05:27 +0200

    Update scripts for Py3.3

--- morse/scripts/ias_kitchen_pr2.sh
+++ morse/scripts/ias_kitchen_pr2.sh
@@ -13,11 +13,11 @@ ROSLIB_DIR=$(rospack find roslib)
 BLENDER_DIR=$(rospack find morse_blender)
 
 # set python version for morse
-PYTHON_VERSION=python3.2
+PYTHON_VERSION=python3.3
 
 if [ -z "$PYTHON3_EXECUTABLE" ]; then
-    if which python3.2; then
-        PYTHON3_EXECUTABLE=`which python3.2`
+    if which python3.3; then
+        PYTHON3_EXECUTABLE=`which python3.3`
     else
         echo "Could not find python3 executable. Please set the environment variable PYTHON3_EXECUTABLE to point to it."
         exit 1
@@ -25,7 +25,7 @@ if [ -z "$PYTHON3_EXECUTABLE" ]; then
 fi
 
 PYTHON_INST_DIR=`$PYTHON3_EXECUTABLE -c "import distutils.sysconfig, sys; sys.stdout.write(distutils.sysconfig.get_python_lib(0,0,''))"`
-export PYTHONPATH=$PYYAML_DIR/pyyaml/lib/python3.2/site-packages:$MORSE_DIR/morse/$PYTHON_INST_DIR:/usr/$PYTHON_INST_DIR:/opt/ros/fuerte/lib/python2.7/dist-packages
+export PYTHONPATH=$PYYAML_DIR/pyyaml/lib/python3.3/site-packages:$MORSE_DIR/morse/$PYTHON_INST_DIR:/usr/$PYTHON_INST_DIR:/opt/ros/fuerte/lib/python2.7/dist-packages
 
 export MORSE_BLENDER=$BLENDER_DIR/bin/blender
 
